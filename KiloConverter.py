2b6b9eae7a3436d236e797a36beb587a9c9dc1b5
Christopher Carr <38837941+ChrisKCarr@users.noreply.github.com>
2018-10-24 17:37:37 -0400

    Update KiloConverter.py

--- KiloConverter.py
+++ KiloConverter.py
@@ -14,22 +14,31 @@ def from_kg():
     t3.insert(END,ounce)
 
 e1=Label(window,text="Kg")
-e1.grid(row=0,column=0)
+e1.grid(row=0,column=1)
+
+g1=Label(window,text="grams")
+g1.grid(row=2,column=0)
+
+p1=Label(window,text="pounds")
+p1.grid(row=2,column=1)
+
+o1=Label(window,text="ounces")
+o1.grid(row=2,column=2)
 
 e2_value=StringVar()
 e2=Entry(window,textvariable=e2_value)
-e2.grid(row=0,column=1)
+e2.grid(row=1,column=1)
 
 b1=Button(window,text="Convert",command=from_kg)
 b1.grid(row=0,column=2)
 
 t1=Text(window,height=1,width=20)
-t1.grid(row=1,column=0)
+t1.grid(row=3,column=0)
 
 t2=Text(window,height=1,width=20)
-t2.grid(row=1,column=1)
+t2.grid(row=3,column=1)
 
 t3=Text(window,height=1,width=20)
-t3.grid(row=1,column=2)
+t3.grid(row=3,column=2)
 
 window.mainloop()
